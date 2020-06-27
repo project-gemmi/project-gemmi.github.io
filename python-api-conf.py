3a44d8eb1082b713170ff670d149ff1518f8214f
Marcin Wojdyr <wojdyr@gmail.com>
2020-06-27 22:30:41 +0200

    replace link to modules.html with link to index.html

--- python-api-conf.py
+++ python-api-conf.py
@@ -9,7 +9,7 @@ MAIN_PROJECT_URL = ".."
 PYBIND11_COMPATIBILITY = True
 
 LINKS_NAVBAR1 = [
-    ('Modules', 'modules', []),
+    ('Contents', 'index', []),
     ('Classes', 'classes', []),
     ('GitHub', 'https://github.com/project-gemmi/gemmi/', []),
     ('Docs', 'https://gemmi.readthedocs.io/', []),
