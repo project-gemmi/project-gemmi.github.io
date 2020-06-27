a092ce9a4a62485c738d56a572968a0ecfd3e999
Marcin Wojdyr <wojdyr@gmail.com>
2020-06-27 22:02:38 +0200

    added m.css configuration for generating python API
    based on https://github.com/project-gemmi/gemmi/pull/27

---
+++ python-api-conf.py
@@ -0,0 +1,18 @@
+# m.css configuration
+
+PROJECT_TITLE = "Gemmi"
+PROJECT_SUBTITLE = "Python API"
+
+INPUT_MODULES = ['gemmi']
+
+MAIN_PROJECT_URL = ".."
+PYBIND11_COMPATIBILITY = True
+
+LINKS_NAVBAR1 = [
+    ('Modules', 'modules', []),
+    ('Classes', 'classes', []),
+    ('GitHub', 'https://github.com/project-gemmi/gemmi/', []),
+    ('Docs', 'https://gemmi.readthedocs.io/', []),
+]
+
+OUTPUT = "python-api"
